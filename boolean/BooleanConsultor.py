dcd95515bd0f8c1ccc7b35c39b2ed4f7a6a29c03
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-05-07 19:32:24 -0400

    Implemented relaxed form of Consult method for BooleanConsultor class

--- boolean/BooleanConsultor.py
+++ boolean/BooleanConsultor.py
@@ -1,5 +1,13 @@
 from basics.BasicConsultor import BasicConsultor
 
 class BooleanConsultor(BasicConsultor):
-    def Consult(self, documents, query):
-        return [i for i in documents if i in query]
+    def Consult(self, documents, query, relaxed=None):
+        if relaxed is None: return [i for i in documents if i in query]
+        relevant = []
+        for i in documents:
+            mx = len(i)
+            for j in query:
+                temp = sum((1 if (x == 0 and y == 1) else 0 for x, y in zip(i, j)))
+                mx = min(mx, temp)
+            if mx <= relaxed: relevant.append(i)
+        return relevant
