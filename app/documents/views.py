7aa5d743a20602b6905ed0df80de75c13cf5633d
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-05-13 22:46:29 -0400

    Fixed views

--- app/documents/views.py
+++ app/documents/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from core.InitializeBooleanModel import InitializeBooleanModel
+from core.InitializeVectorialModel import InitializeVectorialModel
+from core.InitializeProbabilisticModel import InitializeProbabilisticModel
 from core.basics import Consult
 
+boolean = InitializeBooleanModel()
+vectorial = InitializeVectorialModel()
+probabilistic = InitializeProbabilisticModel()
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -13,16 +18,19 @@ def SearchResultsView(request):
     template_name = 'search_results.html'
     q = request.GET.get("q")
     model = request.GET.get("model")
-    data = {}
+
     print("Query = ", q, " Model = ", model)
 
     query = Consult(id=1, content=q)
 
     if model == "Boolean":
-        boolean = InitializeBooleanModel()
-        docs = boolean.Consult(query, relaxed=3)
-        data = {
-            "docs": docs
-        }
+        docs = boolean.Consult(query)
+        data = {"docs": docs}
+    elif model == "Vectorial":
+        docs = vectorial.Consult(query, 20)
+        data = {"docs": docs}
+    else:
+        docs = probabilistic.Consult(query, size=20)
+        data = {"docs": docs}
 
     return render(request, template_name, data)
