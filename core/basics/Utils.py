ee817ee0122ee8a176e9a90ae6a8b5d55a10c92a
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-05-25 23:18:59 -0400

    Fixed angle for vectorial documents to work with numpy dot method

--- core/basics/Utils.py
+++ core/basics/Utils.py
@@ -1,17 +1,7 @@
 from math import sqrt
-
-from numpy import dot as npdot, transpose
-
-
-def dot(u, v):
-    return npdot(u, transpose(v))[0, 0]
+from numpy import dot
 
 def angle(u, v):
     num = dot(u, v)
     if num == 0: return 0
     return num / (sqrt(dot(u, u)) * sqrt(dot(v, v)))
-
-def normalize(arr):
-    mx = max(arr)
-    for i in arr: i /= mx
-    return arr
