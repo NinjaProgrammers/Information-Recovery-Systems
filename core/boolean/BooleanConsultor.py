17f5d45776eca49fe9cfa3a6b249e236a662cbb5
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-05-25 23:15:03 -0400

    Fixed documents for boolean model

--- core/boolean/BooleanConsultor.py
+++ core/boolean/BooleanConsultor.py
@@ -6,11 +6,11 @@ class BooleanConsultor(BasicConsultor):
     def Consult(self, documents, query, relaxed=None):
         relevant = []
         if relaxed is None:
-            relevant = [doc for doc in documents if np.array_equal(doc, query)]
+            relevant = [doc for doc in documents if np.array_equal(doc.tokens, query)]
         else:
             for doc in documents:
-                a = len(np.intersect1d(doc, query))
-                b = len(np.setdiff1d(doc, query))
-                c = len(np.setdiff1d(query, doc))
+                a = len(np.intersect1d(doc.tokens, query))
+                b = len(np.setdiff1d(doc.tokens, query))
+                c = len(np.setdiff1d(query, doc.tokens))
                 if a / (a + 2 * c) >= relaxed: relevant.append(doc)
         return relevant
