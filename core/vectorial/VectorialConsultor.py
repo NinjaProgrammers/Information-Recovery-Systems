4b33930b3cd68b20091692adf0b0b9dc1b26a00c
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-05-26 22:24:08 -0400

    Implemented retroalimentation to vectorial model using Rocchio algorithm

--- core/vectorial/VectorialConsultor.py
+++ core/vectorial/VectorialConsultor.py
@@ -1,5 +1,6 @@
 from core.basics.BasicConsultor import BasicConsultor
 from core.basics.Utils import angle
+import numpy as np
 
 
 class VectorialConsultor(BasicConsultor):
@@ -8,3 +9,14 @@ class VectorialConsultor(BasicConsultor):
         cmp = lambda x: angle(x.tokens, query)
         documents.sort(key=cmp, reverse=True)
         return documents
+
+    def ReConsult(self, documents, query, relevant, irrelevant):
+        if relevant is None or irrelevant is None:
+            return self.Consult(documents, query)
+
+        alpha, beta, gamma = 1, 0.7, 0.3
+        rcentroid = np.sum([d for d in relevant], axis=0) / len(relevant)
+        icentroid = np.sum([d for d in irrelevant], axis=0) / len(irrelevant)
+
+        newquery = alpha * query + beta * rcentroid - gamma * icentroid
+        return self.Consult(documents, newquery)
--- core/vectorial/VectorialModel.py
+++ core/vectorial/VectorialModel.py
@@ -19,3 +19,24 @@ class VectorialModel(BasicModel):
         relevant = self.consultor.Consult(documents, processedQuery)
         if size is None or size >= len(relevant): return relevant
         else: return relevant[: size]
+
+    def ReConsult(self, query, relevant=None, irrelevant=None, size=None):
+        '''
+        :param query: Consult object
+        :param relevant: array of ints (indices of documents)
+        :param irrelevant: array of ints (indices of documents)
+        :param size: int
+        :return: relevant documents
+        '''
+        processedQuery = self.queryProcessor.ProcessQuery(query)
+        documents = self.storage.GetAllDocuments()
+        if relevant is None or irrelevant is None:
+            return self.consultor.ReConsult(documents, processedQuery)
+
+        relevant = [d.tokens for d in documents if d.id in relevant]
+        irrelevant = [d.tokens for d in documents if d.id in irrelevant]
+        remarkable = self.consultor.ReConsult(documents, processedQuery, relevant, irrelevant)
+        if size is None or size >= len(remarkable):
+            return remarkable
+        else:
+            return remarkable[: size]
