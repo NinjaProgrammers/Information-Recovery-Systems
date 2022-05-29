fc4e750750047d419f234c91154fca803f00c7f9
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-05-29 14:52:55 -0400

    Added ranking to boolean model

--- core/boolean/BooleanConsultor.py
+++ core/boolean/BooleanConsultor.py
@@ -4,13 +4,15 @@ from core.basics.BasicConsultor import BasicConsultor
 
 class BooleanConsultor(BasicConsultor):
     def Consult(self, documents, query, relaxed=None):
-        relevant = []
         if relaxed is None:
-            relevant = [doc for doc in documents if np.array_equal(doc.tokens, query)]
+            return [doc for doc in documents if np.array_equal(doc.tokens, query)]
         else:
-            for doc in documents:
+            def cmp(doc):
                 a = len(np.intersect1d(doc.tokens, query))
                 b = len(np.setdiff1d(doc.tokens, query))
                 c = len(np.setdiff1d(query, doc.tokens))
-                if (a + 2 * c) != 0 and a / (a + 2 * c) >= relaxed: relevant.append(doc)
-        return relevant
+                if a + 2 * c == 0: return 0
+                return a / (a + 2 * c)
+
+            documents.sort(key=cmp, reverse=True)
+            return documents
