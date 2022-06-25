d517ff63076642af297e2d8a0fdb84b2ba836b71
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-06-25 00:02:22 -0400

    Added select dataset dropdown to ui application

--- app/documents/urls.py
+++ app/documents/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, ModelView
+from .views import HomePageView, ModelView, RedirectView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("model/<str:model>", ModelView, name="model"),
+    path("model/<str:modelname>/<str:dataset>", ModelView, name="model"),
+    path("redirect", RedirectView, name="redirect"),
 ]
--- app/documents/views.py
+++ app/documents/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django import forms
 from django.forms import formset_factory
 
-from core.InitializeBooleanModel import InitializeBooleanModel
-from core.InitializeVectorialModel import InitializeVectorialModel
-from core.InitializeProbabilisticModel import InitializeProbabilisticModel
+from core.boolean.BooleanModel import BooleanModel
+from core.vectorial.VectorialModel import VectorialModel
+from core.probabilistic.ProbabilisticModel import ProbabilisticModel
+
+from core.fileProcessing.Loader import *
 from core.basics import Consult
+from core.basics.BasicModel import BasicModel
 from time import time
 
-boolean = InitializeBooleanModel()
-vectorial = InitializeVectorialModel()
-probabilistic = InitializeProbabilisticModel()
+cranUrl = "./Test Collections/Cran/"
+imdbUrl = "./Test Collections/imdb/"
+newsgroupUrl = "./Test Collections/Newsgroups/"
+
+class ModelWrapper:
+    def __init__(self):
+        self.model = None
+        self.modelname = None
+        self.dataset = None
+    def GetModel(self, modelname, dataset):
+        if self.modelname == modelname and self.dataset == dataset:
+            return self.model
+        if dataset == "cran": documents, _ = LoadCranDataset(cranUrl)
+        elif dataset == "imdb": documents = LoadIMDBDataset(imdbUrl)
+        else: documents = LoadNewsgroupDataset(newsgroupUrl)
+
+        if modelname == "Boolean": self.model = BooleanModel(documents)
+        elif modelname == "Vectorial": self.model = VectorialModel(documents)
+        elif modelname == "Probabilistic": self.model = ProbabilisticModel(documents)
+        return self.model
+
+wrapper = ModelWrapper()
 
 class RetroalimentationForm(forms.Form):
     checkbox = forms.BooleanField(required=False)
@@ -21,25 +43,32 @@ class RetroalimentationForm(forms.Form):
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+def RedirectView(request):
+    modelname = request.POST.get('model')
+    dataset = request.POST.get('dataset')
+    return redirect('model', modelname=modelname, dataset=dataset)
 
-def ModelView(request, model):
+
+def ModelView(request, modelname, dataset):
     template_name = 'search_results.html'
+
+    model = wrapper.GetModel(modelname, dataset)
 
     size = request.GET.get("size")
     size = 20 if size is None else int(size)
     q = request.GET.get("q")
     data = {"relaxed": False, "iterations": 1, "retroalimentation": False}
     q = "" if q is None else q
-    print("Query = ", q, " Model = ", model)
+    print("Query = ", q, " Model = ", modelname)
     docs = []
     query = Consult(id=1, content=q)
     begTime = time()
-    if model == "Boolean":
+    if modelname == "Boolean":
         relaxedConsult = request.GET.get('relaxed')
         data['relaxed'] = relaxedConsult == 'on'
         if not q == "":
-            docs = boolean.Consult(query, size=size, relaxed=(relaxedConsult == 'on'))
-    elif model == "Vectorial":
+            docs = model.Consult(query, size=size, relaxed=(relaxedConsult == 'on'))
+    elif modelname == "Vectorial":
         retroalimentation = request.GET.get('retroalimentation')
         data['retroalimentation'] = retroalimentation == 'on'
 
@@ -51,18 +80,19 @@ def ModelView(request, model):
                     if y == 'irrelevant': irrelevant.append(int(x))
                     else: relevant.append(int(x))
                 print(relevant, irrelevant)
-                docs = vectorial.ReConsult(query, size=size, relevant=relevant, irrelevant=irrelevant)
+                docs = model.ReConsult(query, size=size, relevant=relevant, irrelevant=irrelevant)
             else:
-                docs = vectorial.Consult(query, size=size)
+                docs = model.Consult(query, size=size)
     else:
         iterations = request.GET.get('iterations')
         iterations = 1 if iterations is None else int(iterations)
         data['iterations'] = iterations
         if not q == "":
-            docs = probabilistic.Consult(query, size=size, retroalimentation=iterations)
+            docs = model.Consult(query, size=size, retroalimentation=iterations)
 
     data["docs"] = docs
-    data["model"] = model
+    data["model"] = modelname
+    data['dataset'] = dataset
     data["q"] = q
     data["size"] = size
     data["time"] = round(time() - begTime, ndigits=2)
